35693f7c26193a46d3648f5eb37aff9dda120fb3
Chris Brooksbank <brooksbankc@parliament.uk>
2021-01-09 11:28:46 +0000

    Added PlayerActionWithCity type

---
+++ autoCardboard.Pandemic.Domain/PlayerActionWithCity.cs
@@ -0,0 +1,8 @@
+﻿namespace autoCardboard.Pandemic.Domain
+{
+    public class PlayerActionWithCity
+    {
+        public PlayerStandardAction PlayerAction { get; set; }
+        public City City { get; set; }
+    }
+}
