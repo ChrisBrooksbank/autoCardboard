b2612bcbc2e2c5b088130f8726ac9b914cbdbcbf
Chris Brooksbank <brooksbankc@parliament.uk>
2021-01-23 13:14:55 +0000

    added a MQTT message server - hosted on a console app

---
+++ autoCardboard.Message.Server/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using MQTTnet;
+using MQTTnet.Server;
+
+namespace autoCardboard.Message.Server
+{
+
+    // https://github.com/chkr1011/MQTTnet/wiki/Server
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartMQTTServer();
+        }
+
+        static async void StartMQTTServer()
+        {
+            var optionsBuilder = new MqttServerOptionsBuilder()
+                .WithConnectionBacklog(100)
+                .WithDefaultEndpointPort(1884);
+
+            var mqttServer = new MqttFactory().CreateMqttServer();
+            await mqttServer.StartAsync(optionsBuilder.Build());
+
+            Console.WriteLine("AutoCardboard messaging server - press any key to exit.");
+            Console.ReadKey();
+            await mqttServer.StopAsync();
+        }
+    }
+}
