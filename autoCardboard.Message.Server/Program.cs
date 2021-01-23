5daa945a5885ea66902c907e5ca7774524551392
Chris Brooksbank <brooksbankc@parliament.uk>
2021-01-23 16:25:58 +0000

    also created a web messageserver, to attempt to get websocket connections to MQtt working....

--- autoCardboard.Message.Server/Program.cs
+++ autoCardboard.Message.Server/Program.cs
@@ -6,6 +6,7 @@ using MQTTnet.Server;
 namespace autoCardboard.Message.Server
 {
 
+    // this websocket doc : https://github.com/chkr1011/MQTTnet/wiki/Server#websocket-endpoint
     // https://github.com/chkr1011/MQTTnet/wiki/Server
     // https://dzone.com/articles/mqtt-publishing-and-subscribing-messages-to-mqtt-b
     class Program
@@ -23,6 +24,11 @@ namespace autoCardboard.Message.Server
 
             var mqttServer = new MqttFactory().CreateMqttServer();
 
+            // https://github.com/chkr1011/MQTTnet/issues/467
+
+            // https://github.com/chkr1011/MQTTnet/issues/390
+            // need to support websockets ??
+            // https://github.com/chkr1011/MQTTnet/issues/680
             mqttServer.UseClientConnectedHandler(ClientConnectedHandler);
 
             await mqttServer.StartAsync(optionsBuilder.Build());
