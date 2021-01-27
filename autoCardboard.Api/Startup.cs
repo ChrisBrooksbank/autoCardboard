5ed8df174e6a41d74f730c2a5b3b477ccf74ecfc
Chris Brooksbank <brooksbankc@parliament.uk>
2021-01-27 11:42:51 +0000

    added swagger to API

--- autoCardboard.Api/Startup.cs
+++ autoCardboard.Api/Startup.cs
@@ -1,8 +1,10 @@
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace autoCardboard.Api
 {
@@ -18,12 +20,36 @@ namespace autoCardboard.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers().AddNewtonsoftJson();
+           services
+               .AddControllers()
+               .AddNewtonsoftJson()
+               .AddJsonOptions(opts => opts.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()) );
+
+           services.AddSwaggerGen(c =>
+           {
+               c.SwaggerDoc("v1", new OpenApiInfo
+               {
+                   Version = "v1",
+                   Title = "AutoCardBoard API",
+                   Description = "Allows boardgames to be played by bots",
+                   Contact = new OpenApiContact() { Name = "Chris Brooksbank", Email = "chrisbrooksbank@gmail.com" }
+               });
+           });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
