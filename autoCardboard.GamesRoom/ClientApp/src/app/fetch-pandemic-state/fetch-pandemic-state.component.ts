95546c64d165ad173214e1e77ccc260580ef2a39
Chris Brooksbank <brooksbankc@parliament.uk>
2021-02-06 09:39:39 +0000

    GamesRoom now calls API ( url hardcoded ) - removed dependancies

--- autoCardboard.GamesRoom/ClientApp/src/app/fetch-pandemic-state/fetch-pandemic-state.component.ts
+++ autoCardboard.GamesRoom/ClientApp/src/app/fetch-pandemic-state/fetch-pandemic-state.component.ts
@@ -9,9 +9,8 @@ export class FetchPandemicStateComponent {
   public pandemicState: PandemicState;
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<PandemicState>(baseUrl + 'Game').subscribe(result => {
-      this.pandemicState = result;
-      console.log(result);
+    http.get<string>(baseUrl + 'Game').subscribe(result => {    
+        this.pandemicState = JSON.parse(result);
     }, error => console.error(error));
   }
 }
--- autoCardboard.GamesRoom/Controllers/GameController.cs
+++ autoCardboard.GamesRoom/Controllers/GameController.cs
@@ -1,14 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using autoCardboard.Common;
-using autoCardboard.Infrastructure;
-using autoCardboard.Messaging;
-using autoCardboard.DependencyInjection;
-using autoCardboard.Pandemic.Game;
-using autoCardboard.Pandemic.State;
-using autoCardboard.Pandemic.TurnState;
+﻿using System.Net.Http;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
 
 namespace autoCardboard.GamesRoom.Controllers
 {
@@ -16,27 +9,17 @@ namespace autoCardboard.GamesRoom.Controllers
    [Route("[controller]")]
     public class GameController : ControllerBase
     {
-        private readonly IServiceProvider _serviceProvider;
-        private readonly ICardboardLogger _logger;
-        private readonly IMessageSender _messageSender;
-
-        public GameController()
-        {
-            _serviceProvider = ServiceProviderFactory.GetServiceProvider();
-            _logger = _serviceProvider.GetService<ICardboardLogger>();
-            _messageSender = _serviceProvider.GetService<IMessageSender>();
-        }
 
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [Produces("application/json")]
-        public IActionResult Get()
+        public async Task<IActionResult> Get()
         {
-            var playerConfiguration = new PlayerConfiguration { PlayerCount = 2 };
-            var pandemicGame = GameFactory.CreateGame<IPandemicState, IPandemicTurn>(_serviceProvider, playerConfiguration) as PandemicGame;
-            pandemicGame.Setup(pandemicGame.Players);
-            pandemicGame.Play();
-            return new JsonResult(pandemicGame.State);
+            var client = new HttpClient();
+            var response = await client.GetAsync("https://localhost:44387/Play?game=Pandemic");
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            return new JsonResult(responseBody);
         }
 
     }
