fb340a5b631fcd94d1cd8611f5e50f28df0bf576
Chris Brooksbank <brooksbankc@parliament.uk>
2021-03-05 16:46:36 +0000

    knowledge sharing is now happening - WIP needs refactoring plus a little more smarts.

--- autoCardBoard.Pandemic.Bots/KnowledgeShareHelper.cs
+++ autoCardBoard.Pandemic.Bots/KnowledgeShareHelper.cs
@@ -14,17 +14,104 @@ namespace autoCardBoard.Pandemic.Bots
 
         public KnowledgeShare GetSuggestedKnowledgeShare(int currentPlayerId, IPandemicState state)
         {
+            var currentPlayerState = state.PlayerStates[currentPlayerId];
+            var currentPlayerCityCards = currentPlayerState.PlayerHand
+                .Where(c => c.PlayerCardType == PlayerCardType.City).Select(c => (City)c.Value).ToList();
+            var diseaseStates = state.DiscoveredCures;
+            var citiesWithoutCures = state.Cities.Where( n => diseaseStates[n.City.GetDefaultDisease()] == DiseaseState.NotCured)
+                .Select(c => c.City).ToList();
+            var city = currentPlayerState.Location;
+            var disease = city.GetDefaultDisease();
             var playersWeCouldShareKnowledgeWith = PlayersWeCouldKnowledgeShareWith(currentPlayerId, state);
+            var weAreAResearcher = currentPlayerState.PlayerRole == PlayerRole.Researcher;
+            var cityCardsWeHaveWithUncuredDiseases = currentPlayerCityCards.Where( c => citiesWithoutCures.Contains(c)).ToList();
 
+            // if we are a researcher, then we can give any city card ( can go other way too, but this will be sufficient for bot )
+            // TODO refactor this incredibly long piece of code ! This was just to understand how to do it, now tidy up !
+            if (weAreAResearcher)
+            {
+               foreach (var playerWeCouldShareKnowledgeWith in playersWeCouldShareKnowledgeWith)
+               {
+                   var candidatePlayerState = state.PlayerStates[playerWeCouldShareKnowledgeWith];
+                   var candidateBlackCount = candidatePlayerState.PlayerHand
+                       .Count(c => c.PlayerCardType == PlayerCardType.City && ((City)c.Value).GetDefaultDisease() == Disease.Black);
+                   var candidateBlueCount = candidatePlayerState.PlayerHand
+                       .Count(c => c.PlayerCardType == PlayerCardType.City && ((City)c.Value).GetDefaultDisease() == Disease.Blue);
+                   var candidateYellowCount = candidatePlayerState.PlayerHand
+                       .Count(c => c.PlayerCardType == PlayerCardType.City && ((City)c.Value).GetDefaultDisease() == Disease.Yellow);
+                   var candidateRedCount = candidatePlayerState.PlayerHand
+                       .Count(c => c.PlayerCardType == PlayerCardType.City && ((City)c.Value).GetDefaultDisease() == Disease.Red);
+
+                   var ourBlackCount = currentPlayerState.PlayerHand
+                       .Count(c => c.PlayerCardType == PlayerCardType.City && ((City)c.Value).GetDefaultDisease() == Disease.Black);
+                   var ourBlueCount = currentPlayerState.PlayerHand
+                       .Count(c => c.PlayerCardType == PlayerCardType.City && ((City)c.Value).GetDefaultDisease() == Disease.Blue);
+                   var ourYellowCount = currentPlayerState.PlayerHand
+                       .Count(c => c.PlayerCardType == PlayerCardType.City && ((City)c.Value).GetDefaultDisease() == Disease.Yellow);
+                   var ourRedCount = currentPlayerState.PlayerHand
+                       .Count(c => c.PlayerCardType == PlayerCardType.City && ((City)c.Value).GetDefaultDisease() == Disease.Red);
+
+                   if (diseaseStates[Disease.Black] == DiseaseState.NotCured && ourBlackCount > 0 && candidateBlackCount > ourBlackCount)
+                   {
+                       return new KnowledgeShare
+                       {
+                           Player1 = currentPlayerId,
+                           Player2 = playerWeCouldShareKnowledgeWith,
+                           CityCardToGive = currentPlayerCityCards.First(c => c.GetDefaultDisease() == Disease.Black)
+                       };
+                   }
+
+                   if (diseaseStates[Disease.Blue] == DiseaseState.NotCured && ourBlueCount > 0 && candidateBlueCount > ourBlueCount)
+                   {
+                       return new KnowledgeShare
+                       {
+                           Player1 = currentPlayerId,
+                           Player2 = playerWeCouldShareKnowledgeWith,
+                           CityCardToGive = currentPlayerCityCards.First(c => c.GetDefaultDisease() == Disease.Blue)
+                       };
+                   }
+
+                   if (diseaseStates[Disease.Yellow] == DiseaseState.NotCured && ourYellowCount > 0 && candidateYellowCount > ourYellowCount)
+                   {
+                       return new KnowledgeShare
+                       {
+                           Player1 = currentPlayerId,
+                           Player2 = playerWeCouldShareKnowledgeWith,
+                           CityCardToGive = currentPlayerCityCards.First(c => c.GetDefaultDisease() == Disease.Yellow)
+                       };
+                   }
+
+                   if (diseaseStates[Disease.Red] == DiseaseState.NotCured && ourRedCount > 0 && candidateRedCount > ourRedCount)
+                   {
+                       return new KnowledgeShare
+                       {
+                           Player1 = currentPlayerId,
+                           Player2 = playerWeCouldShareKnowledgeWith,
+                           CityCardToGive = currentPlayerCityCards.First(c => c.GetDefaultDisease() == Disease.Red)
+                       };
+                   }
+               }
+
+            }
+
+            // if the current city is for a cured or eradicated disease - don't knowledge share
+            if (diseaseStates[disease] != DiseaseState.NotCured)
+            {
+                return null;
+            }
+            
             var playerToShareKnowledgeWith = playersWeCouldShareKnowledgeWith?.FirstOrDefault();
+
             if (playerToShareKnowledgeWith == null)
             {
                 return null;
             }
 
-            // TODO only suggest the share if it benefits the players !!!
-
-
+            // TODO do not return a knowledgeShare unless it improves gameState
+            // Otherwise we may just be knowledge sharing the same card back and forth !
+            // maybe introduce some randomness to knowledge share as well
+            // Similar to researcher above, only move city cards to a player who already has more of that colour than us
+            
             return new KnowledgeShare
             {
                 Player1 = currentPlayerId,
@@ -32,7 +119,7 @@ namespace autoCardBoard.Pandemic.Bots
                 CityCardToGive = state.PlayerStates[currentPlayerId].Location
             };
         }
-
+        
         private List<int> PlayersWeCouldKnowledgeShareWith(int currentPlayerId, IPandemicState state)
         {
             var currentPlayerState = state.PlayerStates[currentPlayerId];
@@ -50,7 +137,7 @@ namespace autoCardBoard.Pandemic.Bots
             }
 
             var weCanShareWithPlayerIds = new List<int>();
-            // If a researcher is in this city, and has at least one city card, then knowledge sharing is possible, he can give ( not take )
+            // If a researcher is in this city, and has at least one city card, then knowledge sharing is possible
             if ( (currentPlayerRole == PlayerRole.Researcher && currentPlayerCityCards.Any())
                  || (otherPlayersInSameCity.Any(p => p.Value.PlayerRole == PlayerRole.Researcher 
                                                      && p.Value.PlayerHand.Any(c => c.PlayerCardType == PlayerCardType.City))))
